caa9851476aae61adf381edc2c2579cebc4973da
manudev97 <manudevcabrera@gmail.com>
2024-12-06 13:30:47 -0500

    test integration

--- staking-contract/src/staking_contract.rs
+++ staking-contract/src/staking_contract.rs
@@ -1,14 +1,22 @@
 #![no_std]
 
 multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+pub const BLOCKS_IN_YEAR: u64 = 60 * 60 * 24 * 365 / 6;
+pub const MAX_PERCENTAGE: u64 = 10_000;
+
+#[derive(TypeAbi, TopEncode, TopDecode, PartialEq, Debug)]
+pub struct StakingPosition<M: ManagedTypeApi> {
+    pub stake_amount: BigUint<M>,
+    pub last_action_block: u64,
+}
 
 #[multiversx_sc::contract]
 pub trait StakingContract {
     #[init]
-    fn init(&self) {
-        self.total_staked().set(BigUint::zero());
-        self.global_reward_index().set(BigUint::zero());
-        self.last_update_timestamp().set(self.blockchain().get_block_timestamp());
+    fn init(&self, apy: u64) {
+        self.apy().set(apy);
     }
 
     #[upgrade]
@@ -20,142 +28,118 @@ pub trait StakingContract {
     #[endpoint]
     fn stake(&self) {
         let payment_amount = self.call_value().egld_value().clone_value();
-        require!(payment_amount > 0, "Your stake must be greater than 0");
+        require!(payment_amount > 0, "Must pay more than 0");
 
         let caller = self.blockchain().get_caller();
-        self.update_global_state();
+        let stake_mapper = self.staking_position(&caller);
 
-        // Recalcular recompensas pendientes del usuario
-        let current_rewards = self.pending_rewards(&caller);
-        self.pending_rewards_mapper(&caller)
-            .set(&current_rewards);
+        let new_user = self.staked_addresses().insert(caller.clone());
+        let mut staking_pos = if !new_user {
+            stake_mapper.get()
+        } else {
+            let current_block = self.blockchain().get_block_epoch();
+            StakingPosition {
+                stake_amount: BigUint::zero(),
+                last_action_block: current_block,
+            }
+        };
 
-        // Actualizar la posición de staking del usuario
-        let user_stake = self.staking_position(&caller).get();
-        self.staking_position(&caller).set(&(user_stake + &payment_amount));
-        self.reward_index(&caller)
-            .set(&self.global_reward_index().get());
+        self.claim_rewards_for_user(&caller, &mut staking_pos);
+        staking_pos.stake_amount += payment_amount;
 
-        // Actualizar el total staked
-        let total_staked = self.total_staked().get();
-        self.total_staked().set(&(total_staked + payment_amount));
+        stake_mapper.set(&staking_pos);
     }
 
     #[endpoint]
     fn unstake(&self, opt_unstake_amount: OptionalValue<BigUint>) {
         let caller = self.blockchain().get_caller();
-        self.update_global_state();
+        self.require_user_staked(&caller);
+
+        let stake_mapper = self.staking_position(&caller);
+        let mut staking_pos = stake_mapper.get();
 
         let unstake_amount = match opt_unstake_amount {
             OptionalValue::Some(amt) => amt,
-            OptionalValue::None => self.staking_position(&caller).get(),
+            OptionalValue::None => staking_pos.stake_amount.clone(),
         };
-
-        let user_stake = self.staking_position(&caller).get();
         require!(
-            unstake_amount > 0 && unstake_amount <= user_stake,
+            unstake_amount > 0 && unstake_amount <= staking_pos.stake_amount,
             "Invalid unstake amount"
         );
 
-        // Recalcular recompensas pendientes del usuario
-        let current_rewards = self.pending_rewards(&caller);
-        self.pending_rewards_mapper(&caller)
-            .set(&current_rewards);
+        self.claim_rewards_for_user(&caller, &mut staking_pos);
+        staking_pos.stake_amount -= &unstake_amount;
 
-        // Actualizar la posición de staking del usuario
-        self.staking_position(&caller)
-            .set(&(user_stake - &unstake_amount));
-        self.reward_index(&caller)
-            .set(&self.global_reward_index().get());
+        if staking_pos.stake_amount > 0 {
+            stake_mapper.set(&staking_pos);
+        } else {
+            stake_mapper.clear();
+            self.staked_addresses().swap_remove(&caller);
+        }
 
-        // Actualizar el total staked
-        let total_staked = self.total_staked().get();
-        self.total_staked().set(&(total_staked - &unstake_amount));
-
-        // Transferir EGLD de vuelta al usuario
         self.send().direct_egld(&caller, &unstake_amount);
     }
 
-    #[endpoint]
+    #[endpoint(claimRewards)]
     fn claim_rewards(&self) {
         let caller = self.blockchain().get_caller();
-        self.update_global_state();
+        self.require_user_staked(&caller);
 
-        // Obtener recompensas pendientes y reiniciarlas
-        let rewards = self.pending_rewards(&caller);
-        require!(rewards > 0, "No rewards available to claim");
+        let stake_mapper = self.staking_position(&caller);
+        let mut staking_pos = stake_mapper.get();
+        self.claim_rewards_for_user(&caller, &mut staking_pos);
 
-        self.pending_rewards_mapper(&caller).set(BigUint::zero());
-        self.reward_index(&caller)
-            .set(&self.global_reward_index().get());
-
-        // Transferir recompensas al usuario
-        self.send().direct_egld(&caller, &rewards);
+        stake_mapper.set(&staking_pos);
     }
 
-    #[view(getPendingRewards)]
-    fn pending_rewards(&self, caller: &ManagedAddress) -> BigUint {
-        let user_stake = self.staking_position(caller).get();
-        if user_stake == 0 {
+    fn require_user_staked(&self, user: &ManagedAddress) {
+        require!(self.staked_addresses().contains(user), "Must stake first");
+    }
+
+    fn claim_rewards_for_user(
+        &self,
+        user: &ManagedAddress,
+        staking_pos: &mut StakingPosition<Self::Api>,
+    ) {
+        let reward_amount = self.calculate_rewards(staking_pos);
+        let current_block = self.blockchain().get_block_nonce();
+        staking_pos.last_action_block = current_block;
+
+        if reward_amount > 0 {
+            self.send().direct_egld(user, &reward_amount);
+        }
+    }
+
+    fn calculate_rewards(&self, staking_position: &StakingPosition<Self::Api>) -> BigUint {
+        let current_block = self.blockchain().get_block_nonce();
+        if current_block <= staking_position.last_action_block {
             return BigUint::zero();
         }
 
-        let current_index = self.global_reward_index().get();
-        let user_index = self.reward_index(caller).get();
-        let accrued_rewards = user_stake * (current_index - user_index);
-        accrued_rewards + self.pending_rewards_mapper(caller).get()
+        let apy = self.apy().get();
+        let block_diff = current_block - staking_position.last_action_block;
+
+        &staking_position.stake_amount * apy / MAX_PERCENTAGE * block_diff / BLOCKS_IN_YEAR
     }
 
-    #[only_owner]
-    #[endpoint]
-    fn update_global_state(&self) {
-        let current_timestamp = self.blockchain().get_block_timestamp();
-        let last_timestamp = self.last_update_timestamp().get();
-        if current_timestamp == last_timestamp {
-            return;
-        }
-
-        let time_elapsed = current_timestamp - last_timestamp;
-        let total_staked = self.total_staked().get();
-
-        if total_staked > 0 {
-            let reward_rate = BigUint::from(3u64); // 0.0003 EGLD por segundo (ajustado a escala)
-            let rewards_generated = reward_rate * time_elapsed;
-            let reward_per_share = rewards_generated / total_staked;
-
-            self.global_reward_index()
-                .update(|index| *index += reward_per_share);
-        }
-
-        self.last_update_timestamp().set(current_timestamp);
+    #[view(calculateRewardsForUser)]
+    fn calculate_rewards_for_user(&self, addr: ManagedAddress) -> BigUint {
+        let staking_pos = self.staking_position(&addr).get();
+        self.calculate_rewards(&staking_pos)
     }
-
-    // Mappers de almacenamiento
-    #[view(getStakingPosition)]
-    #[storage_mapper("stakingPosition")]
-    fn staking_position(&self, addr: &ManagedAddress) -> SingleValueMapper<BigUint>;
 
     #[view(getStakedAddresses)]
     #[storage_mapper("stakedAddresses")]
     fn staked_addresses(&self) -> UnorderedSetMapper<ManagedAddress>;
 
-    #[view(getPendingRewardsMapper)]
-    #[storage_mapper("pendingRewards")]
-    fn pending_rewards_mapper(&self, addr: &ManagedAddress) -> SingleValueMapper<BigUint>;
+    #[view(getStakingPosition)]
+    #[storage_mapper("stakingPosition")]
+    fn staking_position(
+        &self,
+        addr: &ManagedAddress,
+    ) -> SingleValueMapper<StakingPosition<Self::Api>>;
 
-    #[view(getRewardIndex)]
-    #[storage_mapper("rewardIndex")]
-    fn reward_index(&self, addr: &ManagedAddress) -> SingleValueMapper<BigUint>;
-
-    #[view(getTotalStaked)]
-    #[storage_mapper("totalStaked")]
-    fn total_staked(&self) -> SingleValueMapper<BigUint>;
-
-    #[view(getGlobalRewardIndex)]
-    #[storage_mapper("globalRewardIndex")]
-    fn global_reward_index(&self) -> SingleValueMapper<BigUint>;
-
-    #[view(getLastUpdateTimestamp)]
-    #[storage_mapper("lastUpdateTimestamp")]
-    fn last_update_timestamp(&self) -> SingleValueMapper<u64>;
+    #[view(getApy)]
+    #[storage_mapper("apy")]
+    fn apy(&self) -> SingleValueMapper<u64>;
 }
